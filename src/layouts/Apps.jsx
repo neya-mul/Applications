8d36af2d8837f6c8dc7fab37f2b3fea5a1ec55c7
neya-mul <neyamulislam946@gmail.com>
2026-04-10 16:34:44 +0600

    all apps loded successfully

--- src/layouts/Apps.jsx
+++ src/layouts/Apps.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { use } from 'react'
+const appPromise = fetch('/data.json')
+    .then(res => res.json())
+
+
+
 
 export default function Apps() {
+    const apps = use(appPromise)
+   
     return (
         <div>
             <div className='container mx-auto'>
-                <div className='text-center'>
-                    <h1 className='text-3xl'>Trending Apps</h1>
+                <div className='text-center py-10 text-purple-400'>
+                    <h1 className='text-4xl font-bold my-1'>Trending Apps</h1>
                     <p>Explore All Trending Apps on the Market developed by us</p>
+                </div>
+                <div>
+
                 </div>
             </div>
 
