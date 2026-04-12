7a712ad50e990b80997762e6b460c819ac29ed94
neya-mul <neyamulislam946@gmail.com>
2026-04-12 08:51:33 +0600

    bug fixrd

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { Outlet } from "react-router"
+import { Outlet, useLocation } from "react-router"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import { useEffect } from "react"
 
 function App() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+  }, [pathname])
 
 
   return (
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,19 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import Banner from '../layouts/Banner'
 import Rating from '../layouts/Rating'
 import Apps from '../layouts/Apps'
 import { BounceLoader, PacmanLoader } from 'react-spinners'
+// import ScrollToTop from '../contexts/ScrollToTop'  
+// import { useLocation } from 'react-router'
 
 export default function HomePage() {
+  
   return (
     <div>
 
       <Banner></Banner>
       <Rating></Rating>
-      <Suspense fallback={<div className='flex justify-center items-center x'><BounceLoader color='white'/></div>}>
+      <Suspense fallback={<div className='flex justify-center items-center x'><BounceLoader color='white' /></div>}>
         <Apps></Apps>
       </Suspense>
 
